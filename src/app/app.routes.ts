60b6d460ae90bf3fa8afa23ab287dd389b2bd90c
Fran García <fgarciadevelop@gmail.com>
2025-04-07 15:29:16 +0200

    other components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ListComponent } from './components/list/list.component';
+import { LiveComponent } from './components/live/live.component';
+import { StreamingComponent } from './components/streaming/streaming.component';
 
 export const routes: Routes = [
+    { path: 'list', component: ListComponent },
+    { path: 'streaming', component: StreamingComponent },
+    { path: 'live', component: LiveComponent },
     { path: '**', component: HomeComponent },
 ];
