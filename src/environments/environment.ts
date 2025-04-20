86aad193551cf80ed43a51ff3b0842469213d4d6
Fran García <fgarciadevelop@gmail.com>
2025-04-20 22:24:48 +0200

    change to new firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyAU1kEKFhTnkaRqln8OXPuf51NC8zbtWE8",
-        authDomain: "ffsj-form-candidatas.firebaseapp.com",
-        projectId: "ffsj-form-candidatas",
-        storageBucket: "ffsj-form-candidatas.appspot.com",
-        messagingSenderId: "634731027188",
-        appId: "1:634731027188:web:8c1a3d4138c7b4676d9ca1",
-        databaseURL: "https://ffsj-form-candidatas-default-rtdb.europe-west1.firebasedatabase.app"
+        apiKey: "AIzaSyBi5uYjNFuRLQJvgYI45lL4mCjsN6C9f8w",
+        authDomain: "ffsj-live.firebaseapp.com",
+        databaseURL: "https://ffsj-live-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "ffsj-live",
+        storageBucket: "ffsj-live.firebasestorage.app",
+        messagingSenderId: "512214305247",
+        appId: "1:512214305247:web:c7e837b24fdcafa3b6dc6a",
+        measurementId: "G-J1WRG3X0Q0"
     }
 };
